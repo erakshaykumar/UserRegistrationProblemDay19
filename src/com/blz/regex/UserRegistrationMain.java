f20eee2803d1fa703d0939062462a5756c96b7b7
erakshaykumar <akaakshay304@gmail.com>
2022-03-03 22:43:30 +0530

    Added: Welcome Message

---
+++ src/com/blz/regex/UserRegistrationMain.java
@@ -0,0 +1,15 @@
+/*@Purpose : As a User need to enter a valid First Name
+ *  First name starts with Cap and has minimum 3 characters
+ *@File : User Registration Using Regex
+ *@Author : Akshay Kumar
+ */
+
+package com.blz.regex;
+
+public class UserRegistrationMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registation Program");
+	}
+
+}
